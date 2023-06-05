65443923de91f29df95a95c155236b4b472eb827
saharab290 <saharabrmstu1122@gmail.com>
2023-06-05 11:10:51 +0600

    first commit

---
+++ EIS_WEB_Project/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using EIS_WEB_Project.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EIS_WEB_Project.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguraton());
+    }
+}
+public class ApplicationUserEntityConfiguraton : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u  => u.Name).HasMaxLength(256);
+        builder.Property(u => u.Contact).HasMaxLength(256);
+    }
+        
+}
---
+++ EIS_WEB_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EIS_WEB_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
